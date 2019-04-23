b6a86d03f4c49e88838fc1c730eb49be88e238d3
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-04-23 17:00:55 +0200

    :construction: WIP

--- src/Config.php
+++ src/Config.php
@@ -3,12 +3,14 @@
 namespace Jeckel\Gherkin;
 
 use Codeception\Module;
+use Jeckel\Gherkin\FilePath\FilePathAwareInterface;
+use Jeckel\Gherkin\FilePath\FilePathAwareTrait;
 
 /**
  * Class Config
  * @package Jeckel\Gherkin
  */
-class Config extends Module
+class Config extends Module implements FilePathAwareInterface
 {
-
+    use FilePathAwareTrait;
 }
