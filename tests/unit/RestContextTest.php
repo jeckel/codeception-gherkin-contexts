9db22e0058dbdf95578a99740f04efcc988fb03c
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-04-23 18:16:20 +0200

    :white_check_mark: add json from file tests

--- tests/unit/RestContextTest.php
+++ tests/unit/RestContextTest.php
@@ -12,8 +12,13 @@ use Codeception\Lib\ModuleContainer;
 use Codeception\Module\REST;
 use Codeception\Test\Unit;
 use Codeception\Util\Fixtures;
+use Jeckel\Clock\ClockFactory;
 use Jeckel\Gherkin\Config;
+use Jeckel\Gherkin\FilePath\FileHelper;
 use Jeckel\Gherkin\RestContext;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+use org\bovigo\vfs\vfsStreamWrapper;
 use PHPUnit\Framework\MockObject\MockObject;
 
 class RestContextTest extends Unit
@@ -24,18 +29,23 @@ class RestContextTest extends Unit
     /** @var RestContext */
     protected $helper;
 
+    /** @var FileHelper */
+    protected $fileHelper;
+
     /**
      * Setup
      */
     public function setUp()
     {
         $this->rest = $this->createMock(REST::class);
+        $this->fileHelper = $this->createMock(FileHelper::class);
 
         /** @var MockObject | ModuleContainer $moduleContainer */
         $moduleContainer = $this->createMock(ModuleContainer::class);
 
         $this->helper = new RestContext($moduleContainer);
         $this->helper->_inject($this->rest);
+        $this->helper->setFileHelper($this->fileHelper);
 
         return parent::setUp();
     }
@@ -128,5 +138,25 @@ class RestContextTest extends Unit
         $this->rest->expects($this->never())
             ->method('seeResponseContainsJson');
         $this->helper->iShouldSeeResponseContainsJson('foo[]{');
+    }
+
+    /**
+     * @test iShouldSeeResponseContainsJSONFromFile
+     */
+    public function testIShouldSeeResponseContainsJsonFromFile()
+    {
+        $jsonString = '{"foo": ["bar", "baz"]}';
+
+        $root = vfsStream::setup('root', 444, ['content.json' => $jsonString]);
+        $this->fileHelper->expects($this->once())
+            ->method('getAbsolutePathTo')
+            ->with('/content.json', FileHelper::PATH_TO_DATA)
+            ->willReturn($root->url() . '/content.json');
+
+        $this->rest->expects($this->once())
+            ->method('seeResponseContainsJson')
+            ->with(['foo' => ['bar', 'baz']]);
+
+        $this->helper->iShouldSeeResponseContainsJSONFromFile('/content.json');
     }
 }
